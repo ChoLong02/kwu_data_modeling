d7d1cad01985aa30910bec2974788effe2a66535
Chulwoong Choi <sentilemon02@gmail.com>
2023-10-04 12:31:12 +0900

    Update daum_news_one.py

--- ch01_webcrawling/daum_news_one.py
+++ ch01_webcrawling/daum_news_one.py
@@ -16,7 +16,7 @@ from bs4 import BeautifulSoup
 # requests: 웹 사이트(url) 전체 코드 복사 Get
 # BeautifulSoup4: 전체 코드에서 수집하고 싶은 정보만 Get
 
-url = "https://v.daum.net/v/20231004104848652"  # 데이터를 수집하고 싶은 주소
+url = "https://v.daum.net/v/20231004111930039"  # 데이터를 수집하고 싶은 주소
 result = requests.get(url)  # requests는 해당 URL의 모든 소스코드를 가져옴
 # print(result.text)
 
@@ -35,4 +35,18 @@ doc = BeautifulSoup(result.text, "html.parser")
 # get_text() => Tag정보 제거, 순수 Text만 추출
 title = doc.select("h3.tit_view")[0].get_text()
 print(f"제목: {title}")  # f-string 기법
-print("제목: {}".format(title))  # format() 기법
+# print("제목: {}".format(title))  # format() 기법
+
+# section 태그의 자식인 p태그만 선택!
+contents = doc.select("section > p")  # 10개의 P태그 묶음
+# print(len(contents))
+
+# 1.P태그에서 text값만 추출 => 10번
+# 2.10개 text값을 더하기!
+# 3.출력
+content = ""  # 본문 더해서 저장할 변수
+for i, p in enumerate(contents):  # 10개의 p
+    # print(i+1, p.get_text())
+    content = content + p.get_text()
+
+print(f"본문: {content}")
